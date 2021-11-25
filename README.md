9b7848d6cb81dc7bba7d3bec452b397361f89622
Lê Viết Mẫn <manleviet@gmail.com>
2021-11-25 10:11:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FastDiagP_Prover
+ A program proves that FastDiagP's LookAhead function can generate all possible consistency checks needed by FastDiag.
---
+++ src/main/java/at/tugraz/ist/ase/fastdiagp/prover/Prover.java
@@ -0,0 +1,10 @@
+package at.tugraz.ist.ase.fastdiagp.prover;
+
+public class Prover {
+
+    public static void main(String[] args) {
+        // Input: an integer presenting the number of constraints
+
+        // Output:
+    }
+}
